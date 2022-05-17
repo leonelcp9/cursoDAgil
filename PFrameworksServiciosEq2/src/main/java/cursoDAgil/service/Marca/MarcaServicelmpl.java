bac30ee3e36182b661a87dcf6deb28aacd03232d
Cantarranas16 <82000712+Cantarranas16@users.noreply.github.com>
2022-05-16 21:30:09 -0500

    Add files via upload

---
+++ PFrameworksServiciosEq2/src/main/java/cursoDAgil/service/Marca/MarcaServicelmpl.java
@@ -0,0 +1,85 @@
+package cursoDAgil.dao.marca;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Named;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import cursoDAgil.bd.domain.Marca;
+import cursoDAgil.bd.mappers.MarcaMapper;
+
+
+@Named
+public class MarcaServiceImpl implements MarcaService, Serializable {
+	private static final long serialVersionUID = -6456965766968902300L;
+	SqlSession sqlSession;
+	@Autowired
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	public List<Marca> listarMarcas() {
+		// TODO Auto-generated method stub
+		List<Marca> list = null;
+		try {
+			MarcaMapper marcaMapper = sqlSession.getMapper(MarcaMapper.class);
+			list = marcaMapper.listarMarcas();
+			for(Marca m:list) {
+				System.out.println("☃  Id: "+ m.getIdMarca()+"                            ☃");
+				System.out.println("☃  Nombre: "+ m.getNombreMarca());
+				System.out.println("✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺✺");
+				
+			}
+			return list;
+		}catch(Exception e){
+			System.out.println("Error: "+ e);
+		}
+		return null;
+	}
+	
+	public Integer nuevaMarca(Marca nombreMarca) {
+		try{
+		MarcaMapper marcaMapper = sqlSession.getMapper(MarcaMapper.class);
+		return marcaMapper.nuevaMarca(nombreMarca);
+	}catch (Exception e) {
+		System.out.println("Error: " + e);
+	}
+		return null;
+	}
+	
+	public Integer eliminarMarca(Integer idMarca) {
+		try{
+			MarcaMapper marcaMapper = sqlSession.getMapper(MarcaMapper.class);
+			return marcaMapper.eliminarMarca(idMarca);
+		}catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+		
+		return null;
+	}
+	
+	
+	public Integer actualizarMarca(Marca nombreMarca) {
+		try {
+			MarcaMapper marcaMapper = sqlSession.getMapper(MarcaMapper.class);
+			return marcaMapper.actualizarMarca(nombreMarca);
+		}catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+		
+		return null;
+	}
+
+	public Marca obtenerMarcaporId(Map<String, Integer> mapMarca) {
+		try{
+			MarcaMapper marcaMapper = sqlSession.getMapper(MarcaMapper.class);
+	
+			return marcaMapper.obtenerMarcaporId(mapMarca);
+		}catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+		return null;
+	}
+}
